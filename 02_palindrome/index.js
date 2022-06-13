d72c70f383a3222b8481b1a8142e31321ef385d7
somekindofwallflower <someone.ana13@gmail.com>
2022-06-13 22:53:14 +0200

    Add problem description for palindrome

--- 02_palindrome/index.js
+++ 02_palindrome/index.js
@@ -1,3 +1,12 @@
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
 
 // Solution 1
 // Reverse the string and store the result in an variable than compare string to the reversed value
